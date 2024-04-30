51bf6102e7d0809ba055eff6b245d2820c43d3de
qinhao <coderqin@foxmail.com>
2024-04-30 15:51:49 +0800

    组件和props类型约束

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import { useRoutes } from 'react-router-dom'
 import routes from './router'
+import Download from './views/download'
 
 function App() {
-  return <div className="App">{useRoutes(routes)}</div>
+  return (
+    <div className="App">
+      <Download name="zhangjingyi" age={25} height={165}>
+        <div>李峋</div>
+        <span>朱韵</span>
+      </Download>
+      <div className="main">{useRoutes(routes)}</div>
+    </div>
+  )
 }
 
 export default App

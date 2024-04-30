a2ab042e771059e5b5bfc7cfe5e135d5830bb708
qinhao <coderqin@foxmail.com>
2024-04-30 15:23:29 +0800

    路由的基本配置

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import 'normalize.css'
 import '@/assets/css/index.less'
 
 import App from '@/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<App />)
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

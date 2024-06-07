74d42ed0df02d15912982fcdbf7b63ee9e9ae057
qinhao <coderqin@foxmail.com>
2024-06-07 16:11:23 +0800

    推荐页面内容左右的布局

--- src/views/discover/c-views/recommend/style.ts
+++ src/views/discover/c-views/recommend/style.ts
@@ -1,20 +1,27 @@
 import styled from 'styled-components'
 
-export const RecommendWrapper = styled.div``
+export const RecommendWrapper = styled.div`
+  > .content {
+    border: 1px solid #d3d3d3;
+    width: 980px;
+    background-image: url(${require('@/assets/img/wrap-bg.png')});
+    background-size: 103%;
+    display: flex;
+  }
 
-export const RecommendSection = styled.div`
-  border: 1px solid #d3d3d3;
-  width: 980px;
-  background-image: url(${require('@/assets/img/wrap-bg.png')});
-  display: flex;
+  .left {
+    padding: 20px;
+    width: 729px;
+  }
+
+  .right {
+    margin-left: 1px;
+    width: 250px;
+  }
 `
 
-export const RecommendLeft = styled.div`
-  padding: 20px;
-  width: 729px;
-`
+export const RecommendSection = styled.div``
 
-export const RecommendRight = styled.div`
-  margin-left: 1px;
-  width: 250px;
-`
+export const RecommendLeft = styled.div``
+
+export const RecommendRight = styled.div``

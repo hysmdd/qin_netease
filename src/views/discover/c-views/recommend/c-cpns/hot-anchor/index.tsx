12f3d33709650c261ae4997953e010b35da73d56
coderqin <coderqin@foxmail.com>
2024-08-20 21:18:44 +0800

    热门主播区域的内容展示

--- src/views/discover/c-views/recommend/c-cpns/hot-anchor/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/hot-anchor/index.tsx
@@ -1,13 +1,34 @@
 import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
 import { AnchorWrapper } from './style'
+import SectionHeaderV2 from '@/components/section-header-v2'
+import { hotRadios } from '@/assets/data/local-data'
 
 interface IProps {
   children?: ReactNode
 }
 
 const HotAnchor: FC<IProps> = () => {
-  return <AnchorWrapper>HotAnchor</AnchorWrapper>
+  return (
+    <AnchorWrapper>
+      <SectionHeaderV2 title="热门主播" />
+      <div className="anchors">
+        {hotRadios.map((item) => {
+          return (
+            <div className="item" key={item.name}>
+              <a href="" className="image">
+                <img src={item.picUrl} alt="" />
+              </a>
+              <div className="info">
+                <div className="name">{item.name}</div>
+                <div className="position">{item.position}</div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </AnchorWrapper>
+  )
 }
 
 export default memo(HotAnchor)
--- src/views/discover/c-views/recommend/c-cpns/hot-anchor/style.ts
+++ src/views/discover/c-views/recommend/c-cpns/hot-anchor/style.ts
@@ -1,3 +1,41 @@
 import styled from 'styled-components'
 
-export const AnchorWrapper = styled.div``
+export const AnchorWrapper = styled.div`
+  padding: 20px;
+
+  .anchors {
+    margin-top: 20px;
+
+    .item {
+      display: flex;
+      margin-bottom: 10px;
+      width: 210px;
+
+      .image {
+        img {
+          width: 40px;
+          height: 40px;
+        }
+      }
+
+      .info {
+        width: 160px;
+        margin-left: 8px;
+        line-height: 21px;
+        .name {
+          color: #000;
+          font-weight: 700;
+          margin-top: 3px;
+        }
+
+        .position {
+          color: #666;
+
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+        }
+      }
+    }
+  }
+`
